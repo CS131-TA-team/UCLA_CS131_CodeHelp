bfc633300570a3863c8875817d932a6ab8d0c7c5
Patricia Xiao <patricia.xiao@berkeley.edu>
2020-02-28 14:21:01 -0800

    sample usage of aiohttp

---
+++ Python/aiohttp_example.py
@@ -0,0 +1,22 @@
+import asyncio
+import aiohttp
+
+API_KEY = '<your_key>'
+latitude = "+34.068930"
+longtitude = "-118.445127"
+loc = "{0},{1}".format(latitude, longtitude)
+rad = 10
+
+url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?key={0}&location={1}&radius={2}'.format(API_KEY, loc, rad)
+
+async def simple_case(url):
+    async with aiohttp.ClientSession(
+        connector=aiohttp.TCPConnector(
+            ssl=False,
+        ),
+    ) as session:
+        async with session.get(url) as resp:
+            response = await resp.json()
+            print(response)
+
+asyncio.run(simple_case(url))
